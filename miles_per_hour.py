32680078eaea704ece0118e26b956246c42bca33
gunjan <gunjan4699@gmail.com>
2020-05-13 18:02:17 +0530

    Miles_per_Hour_Conversion

--- miles_per_hour.py
+++ miles_per_hour.py
@@ -1,5 +1,8 @@
+# Take input from the user 
 miles_per_hour = float(input(("Welcome to the Miles Per Hour Conversion App\
 	\n\nWhat is your speed in mules per hour: ")))
+
+# 1 mph = 0.4474 mps
 meters_per_second = float(miles_per_hour * 0.4474)
 
 print("Your speed in meters per second is : {}".format(round(meters_per_second, 2)))
