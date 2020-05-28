20263fd5bd57b5e18238c92ca99172efbab43b98
gunjan <gunjan4699@gmail.com>
2020-05-28 17:22:43 +0530

    Favorite Teacher App

--- fav_teachers.py
+++ fav_teachers.py
@@ -1,3 +1,15 @@
+def fav_teacher_fun(fav_teacher):
+	print('\nYour favorite teachers ranked are: ', fav_teacher)
+	sort_fav_teacher = sorted(fav_teacher)
+	print('Your favorite teachers alphabetically are: ', sort_fav_teacher)
+	print('Your favorite teachers in reverse alphabetical order are: ', sort_fav_teacher[::-1])
+
+	print('\nYour top two teachers are: {} and {}.'.format(fav_teacher[0], fav_teacher[1]))
+	print('Your next two favorite teachers are: {} and {}'.format(fav_teacher[2], fav_teacher[3]))
+	print('Your last favorite teacher is: ', fav_teacher[-1])
+	print('You have a total of {} favorite teachers.\n'.format(len(fav_teacher)))
+
+
 print('Welcome to the favorite teachers program.'.title())
 
 fav_teacher = []
@@ -5,41 +17,17 @@ fav_teacher = []
 for i in range(4):
 	fav_teacher.append(input('Who is your {} favorite teacher: '.format(i+1)).title())
 
-print('\nYour favorite teachers ranked are: ', fav_teacher)
-sort_fav_teacher = sorted(fav_teacher)
-print('Your favorite teachers alphabetically are: ', sort_fav_teacher)
-print('Your favorite teachers in reverse alphabetical order are: ', sort_fav_teacher[::-1])
-
-print('\nYour top two teachers are: {} and {}.'.format(fav_teacher[0], fav_teacher[1]))
-print('Your next two favorite teachers are: {} and {}'.format(fav_teacher[2], fav_teacher[3]))
-print('Your last favorite teacher is: ', fav_teacher[-1])
-print('You have a total of {} favorite teachers.\n'.format(len(fav_teacher)))
+fav_teacher_fun(fav_teacher)
 
 new_teacher = input('Oops, '+fav_teacher[0] + ' is no longer your first favorite teacher. Who is your new FAVORITE teacher: ').title()
 print(new_teacher)
 
 fav_teacher.insert(0, new_teacher)
 
-print('\nYour favorite teachers are ranked as : ', fav_teacher)
-sort_fav_teacher = sorted(fav_teacher)
-print('Your favorite teachers alphabetically are: ', sort_fav_teacher)
-print('Your favorite teachers in reverse alphabetical order are: ', sort_fav_teacher[::-1])
-
-print('\nYour top two teachers are: {} and {}.'.format(fav_teacher[0], fav_teacher[1]))
-print('Your next two favorite teachers are: {} and {}'.format(fav_teacher[2], fav_teacher[3]))
-print('Your last favorite teacher is: ', fav_teacher[-1])
-print('You have a total of {} favorite teachers.\n'.format(len(fav_teacher)))
+fav_teacher_fun(fav_teacher)
 
 dislike_teacher = input('You have decided you no longer like a teacher. Which teacher would you like to remove from your list: ').title()
 
 fav_teacher.remove(dislike_teacher)
 
-print('\nYour favorite teachers are ranked as : ', fav_teacher)
-sort_fav_teacher = sorted(fav_teacher)
-print('Your favorite teachers alphabetically are: ', sort_fav_teacher)
-print('Your favorite teachers in reverse alphabetical order are: ', sort_fav_teacher[::-1])
-
-print('\nYour top two teachers are: {} and {}.'.format(fav_teacher[0], fav_teacher[1]))
-print('Your next two favorite teachers are: {} and {}'.format(fav_teacher[2], fav_teacher[3]))
-print('Your last favorite teacher is: ', fav_teacher[-1])
-print('You have a total of {} favorite teachers.\n'.format(len(fav_teacher)))
+fav_teacher_fun(fav_teacher)
